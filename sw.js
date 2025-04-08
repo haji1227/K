daf45b55d18728da6c78497c28ba003d6307eafa
haji1227 <129189745+haji1227@users.noreply.github.com>
2025-04-08 11:22:49 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,34 @@
+const CACHE_NAME = 'my-app-v2'; // ここを変更することでキャッシュ更新
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/icon-192.png',
+  '/icon-512.png',
+  '/manifest.json'
+];
+
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('calc-train-v1').then(cache => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) return caches.delete(key);
+        })
+      )
+    )
+  );
+});
+
+
+
+
 
 self.addEventListener('fetch', event => {
   event.respondWith(
